d942d64ba5944eb9d7a93e9a9e8cb1fd9a280050
ward438 <75048548+ward438@users.noreply.github.com>
2021-04-11 14:22:33 -0500

    created server.js, modified index.js to allow realtime updates of notes after saving, deleting, etc.

---
+++ .history/Develop/server_20210409165532.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const notes = [{
+    routeName: 'notes',
+}]
+
+
+app.use(express.static(__dirname + '/'));
+// // app.use(app.router);
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/notes.html', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+app.get('/edit', (req, res) => res.sendFile(notes));
+
+app.get('/api/notes', (req, res) => {
+    return res.json([{
+        "title": "Test Title",
+        "text": "Test text"
+    }])
+})
+
+app.get('/api/notes/:notes', (req, res) => {
+    const selected = req.params.notes;
+    console.log(selected);
+
+    for (let i = 0; i < notes.length; i++) {
+        if (selected === notes[i].routeName) {
+            return res.json(notes[i]);
+        }
+    }
+    return res.json(false);
+})
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    newNote.routeName = newCharacter.name.replace(/\s+/g, '').toUpperCase();
+    console.log(newNote);
+
+    notes.push(newNote);
+    console.log(newNote);
+})
+
+
+// keep at bottom
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
---
+++ .history/Develop/server_20210411130856.js
@@ -0,0 +1,83 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
+const url = require('url');
+// let obj = JSON.parse(fs.readFileSync('file', 'utf8'));
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(/api/notes);
+
+const notes = [{
+    routeName: 'notes',
+}]
+
+function getDatabaseData() {
+    let jsonData = fs.readFileSync(`./db/db.json`, 'utf8');
+    return JSON.parse(jsonData);
+};
+
+app.use(express.static(__dirname + '/'));
+// // app.use(app.router);
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/notes.html', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+app.get('/edit', (req, res) => res.sendFile(notes));
+
+app.delete('/api/notes/:id', (req, res) => {
+    const selected = req.params.id;
+    console.log(selected);
+    // todo - remove item from the db.json
+    return res.json(false);
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const selected = req.params.id;
+    console.log(selected);
+
+    for (let i = 0; i < notes.length; i++) {
+        if (selected === notes[i].routeName) {
+            return res.json(notes[i]);
+        }
+    }
+    return res.json(false);
+})
+app.get('/api/notes', (req, res) => {
+    return res.json(getDatabaseData());
+})
+app.post('/api/notes', (req, res) => {
+    let data = req.body;
+    console.log(data);
+    return res.json();
+    // return postDatabaseData();
+
+    // todo - create a note object from input
+    // use something to generate the id - probably that uuid library from the otther project
+    // add to the database file
+    // return res.json(<new_note_obj>)
+    // console.log(enterDbdata);
+
+    // // return enterDbdata;
+    // return res.json();
+
+
+
+    // newNote.routeName = newCharacter.name.replace(/\s+/g, '').toUpperCase();
+    // console.log(newNote);
+
+    // notes.push(newNote);
+
+})
+
+// console.log(getDatabaseData());
+
+// keep at bottom
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
